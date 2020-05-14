a0618ce235a4a77558812c3a0ba294b627b6f7d5
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-14 22:59:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_reader :total, :discount
   def initialize(discount = 0)
     @total = 0
-    @discount = discount
+    @discount = discount / 100
   end 
   
   def total=(total)
