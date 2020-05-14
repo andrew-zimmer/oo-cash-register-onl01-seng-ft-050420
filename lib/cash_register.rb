dbd9addcb9fc62ff07bd032df45ca17edd952504
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-14 23:22:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end 
   
   def add_item(title, price, optional = 1)
-    @@items << title * optional
+    @@items << (title * optional).split(title)
     @total = @total + (price.to_f * optional) 
   end 
   
