fa8d5dd62edc4b3c7d96b0727aae8abe1ae5c0e5
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-15 00:14:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -36,7 +36,7 @@ class CashRegister
   def void_last_transaction
     @total = @total - @items[items[-1]][:price]
     if @items[items[-1]][:times].to_i > 1 
-      @items[items[-1]][:times] -1 
+      @items[items[-1]][:times].to_i -1 
     else 
       @items.delete(items[-1])
     end 
